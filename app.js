a1d0ae6f7c8e85b91a8b93d86a85b663827a1f13
tksnate <tksnate@gmail.com>
2023-05-28 15:53:12 -0700

    js update

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const squares = document.querySelectorAll(".square");
 const mole = document.querySelector(".mole");
-const timeLeft = document.querySelector("#time-Left");
+let timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 
 let result = 0;
+let hitPosition;
+let currentTime = 10;
+let timerId = null;
 
 function randomSquare() {
   squares.forEach((square) => {
@@ -12,6 +15,35 @@ function randomSquare() {
 
   let randomSquare = squares[Math.floor(Math.random() * 9)];
   randomSquare.classList.add("mole");
+
+  hitPosition = randomSquare.id;
 }
 
-randomSquare();
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
+
+function moveMoll() {
+  timerId = setInterval(randomSquare, 800);
+}
+
+moveMoll();
+
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    alert("GAME OVER!  Your final score is " + result);
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
